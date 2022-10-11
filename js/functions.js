e8d6213db9fd8339df9d2da2c80cb8fbb1d7954f
César Joel González Oliva <113487509+ChechaWebDesigner@users.noreply.github.com>
2022-10-10 18:50:39 -0600

    Add files via upload

---
+++ js/functions.js
@@ -0,0 +1,92 @@
+export function showEye(descriptionElement) {
+  descriptionElement.classList.toggle("hidden");
+  descriptionElement.classList.toggle("animate-getDown");
+}
+
+export function activeChanges(element, btnUpdate, btnEye, containerDescription, descriptionElement) {
+  element.classList.toggle("hidden");
+  btnUpdate.classList.toggle("hidden");
+  btnEye.classList.toggle("hidden");
+  containerDescription.classList.remove("hidden");
+  containerDescription.classList.add("animate-getDown");
+
+  descriptionElement.setAttribute("contenteditable", true);
+}
+
+export function saveChanges(element, btnEye, btnPencil, descriptionElement) {
+  element.classList.toggle("hidden");
+  btnEye.classList.toggle("hidden");
+  btnPencil.classList.toggle("hidden");
+  descriptionElement.classList.toggle("hidden");
+  descriptionElement.classList.toggle("animate-getDown");
+}
+
+export function validateCeros(num) {
+  return num.toString().padStart(2, 0);
+}
+
+export function evaluateTask(
+  transactionProcess,
+  day,
+  month,
+  year,
+  hour,
+  minute
+) {
+  const readTask = transactionProcess("readonly");
+  const reader = readTask.openCursor();
+
+  reader.addEventListener("success", (e) => {
+    if (!e.target.result) {
+      
+    } else {
+      if (
+        e.target.result.value.day === day &&
+        e.target.result.value.minute === minute &&
+        e.target.result.value.month === month &&
+        e.target.result.value.hour === hour &&
+        e.target.result.value.year === year
+      ) {
+        let nameClass = e.target.result.value.name;
+        const taskFinished = document.querySelector(
+          `.${nameClass.split(" ").join("")}`
+        );
+        const notificacion = new Notification("¡Recordatorio!", {
+          body: `La tarea de ${e.target.result.value.name} ha expirado`,
+          icon: "https://cdn-icons-png.flaticon.com/512/497/497738.png",
+        });
+        taskFinished.classList.add("task-finished");
+      }
+
+      e.target.result.continue();
+    }
+  });
+}
+
+export function taskAvise(transactionProcess) {
+  const actualDate = new Date();
+  const time = 60 - actualDate.getSeconds();
+  setTimeout(() => {
+    checkTask(transactionProcess);
+    setInterval(() => {
+      checkTask(transactionProcess);
+    }, 60000);
+  }, time * 1000);
+}
+
+function checkTask(transactionProcess) {
+  const date = new Date();
+
+  const dateArr = date.toString().split(" ");
+
+  const day = parseFloat(dateArr[2]);
+  const year = parseFloat(dateArr[3]);
+  const dateComplete = dateArr[4].split(":");
+
+  const hour = parseFloat(dateComplete[0]);
+  const minutes = parseFloat(dateComplete[1]);
+
+  const month = date.getMonth();
+
+  evaluateTask(transactionProcess, day, month, year, hour, minutes);
+}
---
+++ js/selects.js
@@ -0,0 +1,99 @@
+/* ------------- Years------------- */
+
+const containerYears = document.getElementById("cmbYears");
+const fragmentYears = document.createDocumentFragment();
+for (let i = 1; i < 5; i++) {
+  const year = document.createElement("option");
+  year.textContent = 2021 + i;
+  year.value = 2021 + i;
+  fragmentYears.appendChild(year);
+}
+containerYears.appendChild(fragmentYears);
+
+/* -------- Months --------------- */
+
+const containerMonths = document.getElementById("cmbMonths");
+const fragmentMonths = document.createDocumentFragment();
+const months = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
+
+for (let i = 0; i < months.length; i++) {
+  const month = document.createElement("option");
+  month.textContent = months[i];
+  month.value = i;
+  fragmentMonths.appendChild(month);
+}
+containerMonths.appendChild(fragmentMonths);
+
+/* --------- Days -------------- */
+
+function daysInAMonth(month, year) {
+  // Es de las pocas funciones que toman en cuenta enero como 1 en vez de 0
+  return new Date(year, month, 0).getDate();
+}
+
+const produceDays = () => {
+  const yearSelected = parseFloat(document.getElementById("cmbYears").value);
+  const monthSelected =
+    parseFloat(document.getElementById("cmbMonths").value) + 1;
+  const days = daysInAMonth(monthSelected, yearSelected);
+  for (let i = 0; i < days; i++) {
+    const day = document.createElement("option");
+    day.textContent = i + 1;
+    day.value = i + 1;
+    fragmentDays.appendChild(day);
+  }
+  containerDays.innerHTML = "";
+  containerDays.appendChild(fragmentDays);
+};
+
+const containerDays = document.getElementById("cmbDays");
+const fragmentDays = document.createDocumentFragment();
+produceDays();
+
+/* Adding Events */
+
+containerMonths.addEventListener("change", (e) => {
+  produceDays();
+});
+
+containerYears.addEventListener("change", (e) => {
+  produceDays();
+});
+
+/* ---------- Hour --------- */
+
+const containerHours = document.getElementById("cmbHours");
+const fragmentsHours = document.createDocumentFragment();
+for (let i = 1; i <= 24; i++) {
+  const hour = document.createElement("option");
+  hour.textContent = i;
+  hour.value = i;
+  fragmentsHours.appendChild(hour);
+}
+containerHours.appendChild(fragmentsHours);
+
+
+/* ------------ Minutes ------------ */
+
+const containerMinutes = document.getElementById("cmbMinutes");
+const fragmentMinutes = document.createDocumentFragment();
+for (let i = 0; i <= 60; i++) {
+  const minutes = document.createElement("option");
+  minutes.textContent = i;
+  minutes.value = i;
+  fragmentMinutes.appendChild(minutes);
+}
+containerMinutes.appendChild(fragmentMinutes);
